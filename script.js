f5b0d8a07b627b00ce84f9c28e276736ddede174
Your Name <you@example.com>
2020-09-01 19:23:00 -0600

    updating randomizer

--- script.js
+++ script.js
@@ -15,20 +15,48 @@ function setGradient() {
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
+
 // Color Randomizer
 
-	let min = 0
-	let max = 255
-	const randomButton = document.getElementById("random-bg");
+let min = 0
+let max = 255
+const randomButton = document.getElementById("random-bg");
 
 const getInteger = () => Math.floor(Math.random() 
 						* (max - min)) + min;
 
+let rgbToHex = function (rgb) { 
+  let hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
+};
+
+let fullColorHex = function(r,g,b) {   
+  let red = rgbToHex(r);
+  let green = rgbToHex(g);
+  let blue = rgbToHex(b);
+  return '#'+red+green+blue;
+};
+
 function triggerRandomBg() {
+	let a = Number(getInteger());
+	let b = Number(getInteger());
+	let c = Number(getInteger());
+	let d = Number(getInteger());
+	let e = Number(getInteger());
+	let f = Number(getInteger());
+
+
+	let randColor1 = `rgb(${a}, ${b}, ${c})`
+	let randColor2 = `rgb(${d}, ${e}, ${f})`
+
 	body.style.background = `linear-gradient(to right, 
-		rgb(${Number(getInteger())}, ${Number(getInteger())}, 
-		${Number(getInteger())}), rgb(${Number(getInteger())}, 
-		${Number(getInteger())}, ${Number(getInteger())})`;
+		${randColor1}, ${randColor2}`;
+
+	color1.value = fullColorHex(a, b, c);
+	color2.value = fullColorHex(d, e, f);		
 
 	css.textContent = `${body.style.background};`;
 }
